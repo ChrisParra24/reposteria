90f55c90782e3cc3ca948b53099f0834fb227481
David_TC <davidtlahuapacoraza.dev@gmail.com>
2021-12-03 22:44:27 -0600

    Propuesta login y panel

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -4,13 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="https://kit.fontawesome.com/8d4425b4c8.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('css/style.css?v=1.10') }}">
     <title>@yield('title')</title>
 </head>
 <body>
+    <header class="panel">
+        <nav class="navbar">
+            <ul class="container-panel">
+                <li class="list-item menu">
+                    <h1><i class="fas fa-birthday-cake home"></i></h1>
+                    <h2 class="menu-responsive"><i class="fas fa-bars"></i></h2>
+                </li>
+                <ul class="navbar-options">
+                    <li class="list-item"><a href="#" class="navbar-link"><i class="fas fa-clipboard-list option-icon"></i>Receta</a></li>
+                    <li class="list-item"><a href="#" class="navbar-link"><i class="fas fa-balance-scale-right option-icon"></i>Ingrediente</a></li>
+                    <li class="list-item"><a href="#" class="navbar-link"><i class="fas fa-hand-holding-usd option-icon"></i>Cotización</a></li>
+                </ul>
+                <div class="user-container"><i class="fas fa-user-circle icon"></i><span>David Tlahuapa Coraza</span></div>
+            </ul>
+        </nav>
+    </header>
 
-    <!--header-->
-    <header>
+    <!-- <header>
         <div class="container-fluid" style="background-color: #F5B7B1">
             <div class="container pt-3">
                 <div class="row justify-content-around align-items-center">
@@ -29,8 +45,7 @@
                 </div>
             </div>
         </div>
-    </header>
-
+    </header> -->
     @yield('content')
 
     <!---->
